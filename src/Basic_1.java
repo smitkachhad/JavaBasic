0e8381d26205de6f04d9119c0cc5ab6e6075d42e
smitk <smitkachhad2000@gmail.com>
2025-03-17 19:57:54 +0530

    init commit

---
+++ src/Basic_1.java
@@ -0,0 +1,9 @@
+class Basic_1 {
+    // Function to demonstrate pattern
+    public static void main(String args[])
+    {
+        System.out.print("hello");
+        System.out.println("hello");
+
+    }
+}
---
+++ src/pattern/P_1.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class P_1 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=i;j<=5;j++)
+            {
+                System.out.println("*");
+            }
+        }
+    }
+}
